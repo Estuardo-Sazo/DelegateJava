a432695b1a632aaf3387ec13bdb4efd7146d5862
Estuardo-Sazo <estuardosazo1206@gmail.com>
2020-04-18 11:26:44 -0600

    Create OperacionesMatematicas.java

---
+++ PatronDelegate/src/patrondelegate/OperacionesMatematicas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patrondelegate;
+
+/**
+ *
+ * @author Jaime Sazo
+ */
+public class OperacionesMatematicas {
+    public double numero1;
+    double numero2;
+    
+     public double dividir(double numero1, double numero2)
+  {
+    return (numero1/numero2);
+  }
+}
