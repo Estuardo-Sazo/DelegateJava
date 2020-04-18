31279e141dfe640db3e1deaaa8722e442dd1667a
Estuardo-Sazo <estuardosazo1206@gmail.com>
2020-04-18 11:26:51 -0600

    Create PatronDelegate.java

---
+++ PatronDelegate/src/patrondelegate/PatronDelegate.java
@@ -0,0 +1,16 @@
+
+package patrondelegate;
+
+/**
+ *
+ * @author Jaime Sazo
+ */
+public class PatronDelegate {
+
+    
+    public static void main(String[] args) {
+        Ingreso miIngreso=new Ingreso();
+         miIngreso.ingresar();
+    }
+    
+}
